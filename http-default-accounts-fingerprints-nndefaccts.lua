1f8f38f599adf2aa8a545a3002924a5d7c1507fd
nnposter <nnposter@users.noreply.github.com>
2026-01-06 15:28:53 -0700

    Add a new fingerprint

--- http-default-accounts-fingerprints-nndefaccts.lua
+++ http-default-accounts-fingerprints-nndefaccts.lua
@@ -4026,6 +4026,23 @@ table.insert(fingerprints, {
 })
 
 table.insert(fingerprints, {
+  name = "HiOSO OLT",
+  category = "routers",
+  paths = {
+    {path = "/"}
+  },
+  target_check = function (host, port, path, response)
+    return (http_auth_realm(response) or ""):lower() == "epon-system"
+  end,
+  login_combos = {
+    {username = "admin",   password = "admin"}
+  },
+  login_check = function (host, port, path, user, pass)
+    return try_http_auth(host, port, path, user, pass, false)
+  end
+})
+
+table.insert(fingerprints, {
   name = "iBall Baton",
   category = "routers",
   paths = {
